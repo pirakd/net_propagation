d1d326a669d64ec28429c3e53572bbe36effeebc
piracy <dpirak@gmail.com>
2021-06-21 16:27:30 +0300

    saving networks as sparse matrices instead of networkx.Graph

--- generate_random_network.py
+++ generate_random_network.py
@@ -1,6 +1,4 @@
-import joblib
-import old_utils as utils
-from old_utils import save_file
+from utils import save_file, read_network
 import networkx as nx
 verbosity = True
 import os
@@ -12,7 +10,7 @@ os.makedirs(output_folder_name, exist_ok=True)
 number_of_networks = 100
 
 #Read the h_sapiens network
-network_graph = utils.read_network("data/H_sapiens.net")
+network_graph = read_network("data/H_sapiens.net")
 
 E = network_graph.number_of_edges()
 Q = 10
@@ -24,5 +22,6 @@ for i in range(number_of_networks):
     print('processing network {}'.format(i))
     H = network_graph.copy()
     nx.swap.double_edge_swap(H, nswap=Q*E, max_tries=Q*E*2)
+    sparse_mat = nx.to_scipy_sparse_matrix(H, weight=2)
     network_file_path = os.path.join(output_folder_name, network_name)
-    save_file(H, network_file_path)
+    save_file(sparse_mat, network_file_path)
