d2714156a10750e0784ce8091b43fa77ea2d1d7a
piracy <dpirak@gmail.com>
2021-06-23 13:47:26 +0300

    added an args class for the project and refactored propagation_main.py

---
+++ args.py
@@ -0,0 +1,35 @@
+from os import path
+from utils import create_output_folder
+from prior_conditions import get_condition_function
+
+class Args:
+    def __init__(self, test_name=None):
+        # ~~~ general parameters ~~~
+        self.root_folder = path.dirname(path.realpath(__file__))
+        self.data_file = 'data'
+        self.network_file = 'H_sapiens.net'
+        self.experiment_file = 'Table_S1_V1.xlsx'
+        self.sheet_name = 'Protein_Abundance'
+        self.condition_function_name = 'kent_mock_no_vic_mock_24h'
+        self.propagation_input_type = 'abs_log2FC'  # ones, logfc
+        self.pathway_file = 'canonical_pathways.txt'
+        self.interesting_pathway_file = 'interesting_pathways.txt'
+        self.random_network_file = 'random_networks'
+
+        # ~~~ derived parameters ~~~
+        if test_name is None:
+            self.test_name = 'classical_enrichment_{}'.format(self.condition_function_name)
+        else:
+            self.test_name = test_name
+
+        self.data_dir = path.join(self.root_folder, 'data')
+        self.output_folder = create_output_folder(self.test_name)
+        self.condition_function = get_condition_function(self.condition_function_name)
+        # get conditions on experiment
+        self.network_file = path.join(self.data_dir, self.network_file)
+        self.experiment_file_path = path.join(self.data_dir, self.experiment_file)
+        self.pathway_file_dir = path.join(self.data_dir, self.pathway_file)
+        self.interesting_pathway_file_dir = path.join(self.data_dir, self.interesting_pathway_file)
+        self.random_network_dir = path.join(self.root_folder, self.random_network_file)
+    def set_condition_function(self):
+        self.condition_function = get_condition_function(self.condition_function_name)
--- propagation_main.py
+++ propagation_main.py
@@ -3,35 +3,19 @@ from utils import read_prior_set, get_propagation_input, create_output_folder
 from os import path
 from propagation_routines import propagate_network, propagate_networks, get_genes_p_values, propagate_networks_parallel
 from prior_conditions import get_condition_function
+from args import Args
+test_name = 'propagation_main'
 
-
-# ~~~ general parameters ~~~
-data_dir = 'data'
-network_file = 'H_sapiens.net'
-experiment_file = 'Table_S1_V1.xlsx'
-sheet_name = 'Protein_Abundance'
-random_networks_dir = 'random_networks'
-condition_function_name = 'kent_mock_no_vic_mock_24h'
-n_networks = 4
-n_processes = 1
-propagation_input_type = 'abs_log2FC' #ones, logfc
-
-# ~~~ derived parameters ~~~
-test_name = '{}_{}'.format(condition_function_name, n_networks)
-output_folder = create_output_folder(test_name)
-condition_function = get_condition_function(condition_function_name)
-# get conditions on experiment
-network_file = path.join(data_dir, network_file)
-experiment_file_path = path.join(data_dir, experiment_file)
+args = Args(test_name)
 
 # Read the h_sapiens network
-network_graph = utils.read_network(network_file)
+network_graph = utils.read_network(args.network_file)
 
 # loading prior set
-prior_set, prior_data = read_prior_set(condition_function, experiment_file_path, sheet_name)
+prior_set, prior_data = read_prior_set(args.condition_function, args.experiment_file_path, args.sheet_name)
 prior_gene_dict = utils.convert_symbols_to_ids(prior_set)
 prior_set_ids = list(prior_gene_dict.keys())
-propagation_input = get_propagation_input(prior_gene_dict, prior_data, propagation_input_type)
+propagation_input = get_propagation_input(prior_gene_dict, prior_data, args.propagation_input_type)
 
 
 # Using the graph, either run the propagation or load previously acquired propagation results
@@ -40,7 +24,7 @@ genes_idx_to_id = {xx: x for x, xx in genes_id_to_idx.items()}
 
 # Propagate using randomized networks
 _, random_networks_scores = propagate_networks(network_graph, list(genes_id_to_idx.keys()), prior_set_ids,
-                                               propagation_input, random_networks_dir, n_networks=n_networks)
+                                               propagation_input, args.random_networks_dir, n_networks=args.n_networks)
 
 # Rank the genes in the original network compared to the random networks
 p_values = get_genes_p_values(gene_scores, random_networks_scores)
@@ -49,9 +33,9 @@ title = ['gene_id\tp_value\n']
 lines = ['{}\t{}\n'.format(genes_idx_to_id[i], p_values[i]) for i in range(len(p_values))]
 lines = title + lines
 
-with open(path.join(output_folder, 'p_values'), 'w') as f:
+with open(path.join(args.output_folder, 'p_values'), 'w') as f:
     f.writelines(lines)
 
 lines = ['{}\n'.format(p) for p in prior_set_ids]
-with open(path.join(output_folder, 'prior_set'), 'w') as f:
+with open(path.join(args.output_folder, 'prior_set'), 'w') as f:
     f.writelines(lines)
