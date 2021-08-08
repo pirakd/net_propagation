298e887510f3818e0a26d3c6f4288d3f7fe1ffcb
pirakd <dpirak@gmail.com>
2021-08-08 21:38:40 +0200

    Huntingtons delivery version of comparison script

--- scripts/comparison_04_07.py
+++ scripts/comparison_04_07.py
@@ -14,7 +14,7 @@ args = Args(test_name)
 colls_names = ['Significant genes proportion test', 'Log2FC signed', 'Propagation signed',
               'Propagation unsigned']
 
-alpha_values_list = [0.8, 0.9]
+alpha_values_list = [0.90, 0.95]
 min_genes_in_pathway = 10
 min_genes_for_display = 3
 
@@ -51,15 +51,14 @@ for i in range(n_tests):
     # get all genes list
     # all_genes, all_data, _ = read_prior_set(huntington_DDA, args.experiment_file_path, args.sheet_name)
     all_genes, all_data, _ = read_prior_set(all_genes_condition, args.experiment_file_path, args.sheet_name)
-    all_genes = list(all_data.Gene_Name)
     all_genes_dict = utils.convert_symbols_to_ids(all_genes, args.genes_names_file_path)
 
     # get excel scores for all genes which appear in the network
-    reference_scores = get_propagation_input(all_genes_dict, all_data, 'Score')
+    reference_scores = get_propagation_input(all_genes_dict, all_data, 'Score', network=network_graph)
     reference_scores = {x: xx for x, xx in reference_scores.items() if x in network_genes_ids}
 
     # load propagation scores
-    args.propagation_input_type = 'abs_score_all'
+    args.propagation_input_type = 'abs_Score_all'
     unsigned_gene_scores, _, _ = load_propagation_scores(args, normalize_score=True)
     args.propagation_input_type = 'Score'
     signed_gene_scores, genes_idx_to_id, genes_id_to_idx = load_propagation_scores(args, normalize_score=True)
@@ -68,8 +67,6 @@ for i in range(n_tests):
     genes_by_pathway_all = load_pathways_genes(args.pathway_file_dir)
     names_to_ids = utils.convert_symbols_to_ids(genes_names_file_path=args.genes_names_file_path)
     ids_to_names = {xx: x for x, xx in names_to_ids.items()}
-    all_pathways_genes_by_name = {pathway: [ids_to_names[id] for id in genes if id in ids_to_names]
-                                 for pathway, genes in genes_by_pathway_all.items()}
 
     # filter pathway according to a defined condition
     genes_by_pathway_interesting = {x: xx for x, xx in genes_by_pathway_all.items() if any(key in x for key in interesting_pathways_keywords)}
@@ -98,13 +95,17 @@ for i in range(n_tests):
     proportion_scores_dict = Results([],[],[])
 
     # propotion test for significant experiment genes
-    proportion_res = proportion_test(significant_genes, all_pathways_genes_by_name, pathways_with_many_genes)
+    network_genes_names = [ids_to_names[id] for id in genes_id_to_idx.keys() if id in ids_to_names]
+    all_pathways_genes_by_name = {pathway: [ids_to_names[id] for id in genes if (id in ids_to_names)]
+                                 for pathway, genes in genes_by_pathway_all.items()}
+    significant_in_net = [gene_name for gene_name in significant_genes if gene_name in network_genes_names]
+    proportion_res = proportion_test(significant_in_net, all_pathways_genes_by_name, pathways_with_many_genes,
+                                     constrain_to_netowrk_genes=True, network_genes=network_genes_names)
     proportion_scores_dict.p_value.extend([proportion_res[pathway] for pathway in pathways_with_many_genes])
     proportion_scores_dict.direction.extend([True] * len(pathways_with_many_genes))
     proportion_scores_dict.adj_p_value.extend(bh_correction(proportion_scores_dict.p_value))
 
     for p,pathway in enumerate(pathways_with_many_genes):
-
         reference_genes = [id for id in reference_scores.keys() if id not in reference_filtered_genes_by_pathway[pathway]]
         reference_background_scores = [reference_scores[id] for id in reference_genes]
         path_scores = reference_filtered_genes_scores_by_filtered_pathways[pathway]
@@ -114,7 +115,6 @@ for i in range(n_tests):
             signed_fc_res = statistic_test(path_scores, reference_background_scores, alternative='two-sided')
             fc_signed_scores_dict.p_value.append(signed_fc_res.p_value)
             fc_signed_scores_dict.direction.append(signed_fc_res.directionality)
-
             dir = np.mean(path_scores) > np.mean(reference_background_scores)
 
         else:
@@ -131,7 +131,8 @@ for i in range(n_tests):
                                   [signed_gene_scores[genes_id_to_idx[id]] for id in prop_random_genes],
                                   alternative='two-sided')
         prop_signed_scores_dict.p_value.append(prop_signed_res.p_value)
-        prop_signed_scores_dict.direction.append(prop_signed_res.directionality)
+        # prop_signed_scores_dict.direction.append(prop_signed_res.directionality)
+        prop_signed_scores_dict.direction.append(dir)
 
         # unsigned propagation Wilcoxon rank sum test
         prop_unsigned_res = statistic_test(prop_filtered_unsigned_genes_scores_by_filtered_pathways[pathway],
@@ -152,13 +153,15 @@ for i in range(n_tests):
         adj_p_vals_mat[:, i] = bh_correction(results[i].p_value)
         directions_mat[:, i] = results[i].direction
 
-    keep_rows = np.nonzero(np.any(adj_p_vals_mat<0.01 , axis=1 ))[0]
-
+    keep_rows = np.nonzero(np.any(adj_p_vals_mat < 0.05, axis=1))[0]
+    title = 'Pathway Enrichment, Only significant paths({}\{})'.format(len(keep_rows), len(pathways_with_many_genes))
     pathways_with_many_genes = [pathways_with_many_genes[x] for x in keep_rows]
+
     p_vals_mat = p_vals_mat[keep_rows, :]
     adj_p_vals_mat = adj_p_vals_mat[keep_rows, :]
     directions_mat = directions_mat[keep_rows, :]
+    directions_mat[:, 0] = directions_mat[:, 1]
     res = -np.log10(p_vals_mat)
     fig_out_dir = path.join(args.output_folder, fig_name)
-    plot_enrichment_table(res ,adj_p_vals_mat, directions_mat, pathways_with_many_genes, fig_out_dir,
+    plot_enrichment_table(res, adj_p_vals_mat, directions_mat, pathways_with_many_genes, fig_out_dir,
                           experiment_names=colls_names, title=title, res_type='-log10(p_val)')
