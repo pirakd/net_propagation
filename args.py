5c8c858e78193eaa2a05bc33c94714be7df744ed
pirakd <dpirak@gmail.com>
2021-09-14 14:40:45 +0300

    added arguments preset classes for each data type

--- args.py
+++ args.py
@@ -5,56 +5,48 @@ from datetime import datetime
 
 
 class Args:
-    def __init__(self, test_name=None, is_create_output_folder=True):
+    def __init__(self, test_name='test', is_create_output_folder=True):
+        # here we define default parameters that will optionally be overwritten by child classes
+
         # ~~~ general parameters ~~~
         self.root_folder = path.dirname(path.realpath(__file__))
-        self.data_file = 'data'
+        self.data_file = 'cov_data'
+        self.network_file = 'H_sapiens.net'
+        self.experiment_file = 'scores'
+        self.propagation_input_type = 'ones'
+        self.sheet_name = 'Table_A'
+        self.condition_function_name = 'cov_data'
+        self.interesting_pathway_file = 'interesting_pathways.txt'
+        self.genes_names_file = 'gen'
 
-        ## corona virus
-        # self.network_file = 'H_sapiens.net'
-        # self.experiment_file = 'Table_S1_V1.xlsx'
-        # self.propagation_input_type = 'abs_Score'
-        # self.sheet_name = 'Protein_Abundance'
-        # self.condition_function_name = 'kent_vic_24h'
-        # self.interesting_pathway_file = 'interesting_pathways.txt'
 
-        ## Huntington
-        self.data_file = 'HD_data'
-        self.network_file = 'HD.net'
-        self.condition_function_name = 'huntington_DDA'
-        self.sheet_name = 'Suppl. Table 4A'
-        self.interesting_pathway_file = 'None'
-
-        ## colorectal_data
-        # self.data_file = 'colorectal_data'
-        # self.network_file = 'H_sapiens.net'
-        # self.experiment_file = 'scores.xlsx'
-        # self.condition_function_name = 'colorectal_cancer'
-        # self.propagation_input_type = 'abs_Score'
-        # self.sheet_name = 'EV_positive'
-        # self.interesting_pathway_file = None
-        
         self.experiment_file = 'scores.xlsx'
-        self.propagation_input_type = 'abs_Score'
         self.pathway_file = 'canonical_pathways.txt'
         self.random_network_file = 'random_networks'
-        self.genes_names_file = 'genes_names_to_ids'
-        # self.genes_names_file = None
         self.n_networks = 1000
         self.propagation_folder = 'propagation_scores'
         self.date = None
 
         # propagation parameters
-        self.alpha = 0.9
+        self.alpha = 0.98
         self.n_max_iterations = 200
-        self.convergence_th = 1e-5
+        self.convergence_th = 1e-6
+        self.test_name = test_name
 
+        self.data_dir = None
+        self.output_folder = None
+        self.condition_function = None
+        self.network_file_path = None
+        self.experiment_file_path = None
+        self.pathway_file_dir = None
+        self.interesting_pathway_file_dir = None
+        self.genes_names_file_path = None
+        self.propagation_scores_path = None
+        self.get_derived_parameters(is_create_output_folder)
 
         # ~~~ derived parameters ~~~
-        if test_name is None:
-            self.test_name = 'classical_enrichment_{}'.format(self.condition_function_name)
-        else:
-            self.test_name = test_name
+    def get_derived_parameters(self, is_create_output_folder=True):
+        self.set_condition_function()
 
         self.data_dir = path.join(self.root_folder, self.data_file)
         if is_create_output_folder:
@@ -65,7 +57,7 @@ class Args:
 
         self.condition_function = get_condition_function(self.condition_function_name)
         # get conditions on experiment
-        self.network_file = path.join(self.data_dir, self.network_file)
+        self.network_file_path = path.join(self.data_dir, self.network_file)
         self.experiment_file_path = path.join(self.data_dir, self.experiment_file)
         self.pathway_file_dir = path.join(self.data_dir, self.pathway_file)
 
@@ -78,6 +70,50 @@ class Args:
             self.genes_names_file_path = None
 
         self.propagation_scores_path = path.join(self.root_folder, self.propagation_folder)
+        return self.data_dir, self.output_folder, self.condition_function, self.network_file, self.experiment_file_path,\
+               self.pathway_file_dir, self.interesting_pathway_file_dir, self.genes_names_file_path, self.propagation_scores_path
 
     def set_condition_function(self):
         self.condition_function = get_condition_function(self.condition_function_name)
+
+
+class CovArgs(Args):
+    def __init__(self, test_name=None, is_create_output_folder=True):
+        super().__init__(is_create_output_folder=False)
+        self.network_file = 'H_sapiens.net'
+        self.sheet_name = 'Table_A'
+        self.propagation_input_type = 'Score'
+        self.condition_function_name = 'cov_data'
+        self.interesting_pathway_file = 'interesting_pathways.txt'
+        self.genes_names_file = 'genes_names_H_sapiens'
+        self.get_derived_parameters(is_create_output_folder=True)
+
+class HDArgs(Args):
+    def __init__(self, test_name=None, is_create_output_folder=True):
+        super().__init__(is_create_output_folder=False)
+        self.data_file = 'HD_data'
+        self.network_file = 'HD.net'
+        self.condition_function_name = 'huntington_DDA'
+        self.sheet_name = 'Suppl. Table 4A'
+        self.interesting_pathway_file = 'None'
+        self.genes_names_file = 'genes_names_HD'
+        self.get_derived_parameters(is_create_output_folder=True)
+
+class CollerectaArgs(Args):
+    def __init__(self, test_name=None, is_create_output_folder=True):
+        super().__init__(is_create_output_folder=False)
+        self.data_file = 'colorectal_data'
+        self.network_file = 'H_sapiens.net'
+        self.experiment_file = 'scores.xlsx'
+        self.condition_function_name = 'colorectal_cancer'
+        self.propagation_input_type = 'abs_Score'
+        self.sheet_name = 'EV_positive'
+        self.interesting_pathway_file = None
+        self.get_derived_parameters(is_create_output_folder=True)
+
+if __name__ == '__main__':
+    # instantiate an argument object with default parameter
+    default_args = Args()
+
+    # instantiate an argument object with covid preset
+    cov_args = CovArgs()
