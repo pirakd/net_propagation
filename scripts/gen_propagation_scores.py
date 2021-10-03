0db744ea5484fc539355fb1a87961ad0be8f3826
pirakd <dpirak@gmail.com>
2021-10-03 15:01:05 +0300

    updated gen_propagation_scores script according to last commits

--- scripts/gen_propagation_scores.py
+++ scripts/gen_propagation_scores.py
@@ -8,15 +8,17 @@ sys.path.append(path.dirname(path.dirname(path.realpath(__file__))))
 import utils as utils
 from utils import read_prior_set, get_propagation_input, save_propagation_score, get_time
 from propagation_routines import propagate_network
-from args import CovArgs
+from args import CovArgs, HDArgs
 
 propagation_results_dir = path.join('output', 'propagation_results')
 args = CovArgs(None, is_create_output_folder=False)
-n_tests = 7
-alpha = [0.6, 0.7, 0.8, 0.9, 0.95,0.98, 1]
-sheet_names = [args.sheet_name] * len(alpha) # ['Suppl. Table 4A'] * len(alpha)
-prior_set_conditions = [args.condition_function_name] * len(alpha) # ['huntington_DDA_significant'] * len(alpha)
-propagation_input_type_list = ['Score'] * len(alpha)
+# args = HDArgs(None, False)
+n_tests = 4
+alpha = [0.8, 1] * 2
+sheet_names = ['Kent_10h-VIC_10h', 'Kent_10h-VIC_10h',
+                'Kent_24h-VIC_24h', 'Kent_24h-VIC_24h'] # ['Suppl. Table 4A'] * len(alpha)
+prior_set_conditions = [args.condition_function_name] * n_tests # ['huntington_DDA_significant'] * len(alpha)
+propagation_input_type_list = ['Score'] * n_tests
 
 
 network_graph = utils.read_network(args.network_file_path)
@@ -24,7 +26,7 @@ fc_scores_dict = dict(p_vals=list(), adj_p_vals=list(), direction=list())
 prop_scores_dict = dict(p_vals=list(), adj_p_vals=list(), direction=list())
 
 # Run:
-for c, condition in enumerate(range(n_tests)):
+for c in range(n_tests):
     # loading arguments
     args.sheet_name = sheet_names[c]
     args.alpha = alpha[c]
@@ -39,13 +41,12 @@ for c, condition in enumerate(range(n_tests)):
     propagation_input = get_propagation_input(prior_gene_dict, prior_data, args.propagation_input_type,
                                               network=network_graph)
     # Propagate network
-    _, _, genes_id_to_idx, gene_scores = propagate_network(network_graph, propagation_input, args,
-                                                           prior_set=list(prior_gene_dict.values()))
+    _, _, genes_id_to_idx, gene_scores, self_propagation = propagate_network(network_graph, propagation_input, args)
     genes_idx_to_id = {xx: x for x, xx in genes_id_to_idx.items()}
 
     prop_score_file_name = '{}_{}_{}_{}'.format(args.propagation_input_type, args.sheet_name, prior_set_conditions[c], str(args.alpha))
 
     # save propagation score
     save_propagation_score(file_name=prop_score_file_name, propagation_scores=gene_scores, prior_set=prior_set,
-                           propagation_input=propagation_input, genes_idx_to_id=genes_idx_to_id, args=args,
+                           propagation_input=propagation_input, self_propagation=self_propagation, genes_idx_to_id=genes_idx_to_id, args=args,
                            date=get_time())
